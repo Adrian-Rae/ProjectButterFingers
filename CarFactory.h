1c9b1045dd1620a96eb5ec0ea7d6a5c4a03e0b11
Siviwe22 <73336783+Siviwe22@users.noreply.github.com>
2020-10-30 11:17:38 +0200

    Add files via upload

---
+++ CarFactory.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef CARFACTORY_H
+#define CARFACTORY_H
+#include "Factory.h"
+#include <iostream>
+
+using namespace std;
+
+class CarFactory : public Factory{
+
+public:
+    CarFactory();
+    virtual Car *buildCar();
+    virtual void fitWheels();
+    void bulidChasis(); 
+    void attachParts();
+};
+
+#endif //CARFACTORY_H
