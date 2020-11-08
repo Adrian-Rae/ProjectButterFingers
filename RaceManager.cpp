cde982f723d32d3a0f34e27ac1d99acc2c968529
TertiusDeJongHH <71032971+TertiusDeJongHH@users.noreply.github.com>
2020-11-08 11:31:01 +0200

    Demo-ready

--- RaceManager.cpp
+++ RaceManager.cpp
@@ -4,91 +4,111 @@
 
 using namespace std;
 
-int main()
+void RaceManager::mainRun()
 {
-    cout << "Welcome to out COS 214 group project!" << endl;
-    cout << "You will be propted to enter inputs to calibrate the simulation" << endl;
-    cout << "Prompts will also appear to slow down the simulation for demo purposes" << endl;
-    cout << "You can enter any value to continue" << endl;
-    cout << "__________________________________________________________________________" << endl;
-    cout << endl;
-    srand(time(0));
-    string dummy;
-    cout << "The season is starting !"<< endl;
-    cout << "__________________________________________________________________________" << endl;
-    cout << endl;
-    int num, teams;
-    cout << "How many races will there be: \t";
-    cin >> num ;
-    cout << "How many teams will there be: \t";
-    cin >> teams;
-    cout << "__________________________________________________________________________" << endl;
-    cout << endl;
+		string start;
+	    cout << "Welcome to out COS 214 group project!" << endl;
+	    cout << "You will be propted to enter inputs to calibrate the simulation" << endl;
+	    cout << "Prompts will also appear to slow down the simulation for demo purposes" << endl;
+	    cout<<"The prompts will guid you as you go ahead."<<endl;
+	    cout<<"Shall we start? Type 'Y' or 'N' "<<endl;
+	    cin>>start;
+	if(start == "Y" || start == "y"){
+		    cout << "__________________________________________________________________________" << endl;
+		    cout << endl;
+		    srand(time(0));
+		    string dummy;
+		    cout << "The season is starting !"<< endl;
+		    cout << "__________________________________________________________________________" << endl;
+		    cout << endl;
+		    int num, teams;
+		    cout << "How many races will there be: \t";
+		    cin >> num ;
+		    cout << "How many teams will there be: \t";
+		    cin >> teams;
+		    cout << "__________________________________________________________________________" << endl;
+		    cout << endl;
 
-    RaceBuilder* builder = new RaceBuilder();
+		    RaceBuilder* builder = new RaceBuilder();
 
 
-    cout << "**\tFirst this year's season of F1 needs to be planned\t**" << endl;
-    cout << "__________________________________________________________________________" << endl;
-    cout << endl;
+		    cout << "**\tFirst this year's season of F1 needs to be planned\t**" << endl;
+		    cout << "__________________________________________________________________________" << endl;
+		    cout << endl;
 
-    builder->build(num, teams);
-    RaceCatelogue* races = builder->getResult();
-    RaceIterator* iterator = races->createIterator();
-    Race* current = iterator->first();
+		    builder->build(num, teams);
+		    RaceCatelogue* races = builder->getResult();
+		    RaceIterator* iterator = races->createIterator();
+		    Race* current = iterator->first();
 
-    RaceManager* manager = new RaceManager();
+		    RaceManager* manager = new RaceManager();
 
-    int* results;
+		    int* results;
 
-    int** sorting = new int*[teams];
-    for (int i = 0; i < teams; i++){
-        sorting[i] = new int[2];
-        sorting[i][0] = i;
-        sorting[i][1] = 0;
-    }
+		    int** sorting = new int*[teams];
+		    for (int i = 0; i < teams; i++){
+			sorting[i] = new int[2];
+			sorting[i][0] = i;
+			sorting[i][1] = 0;
+		    }
 
-    int** standings = new int* [teams];
-    for (int i = 0; i < teams; i++) {
-        standings[i] = new int[2];
-        standings[i][0] = i;
-        standings[i][1] = 0;
-    }
+		    int** standings = new int* [teams];
+		    for (int i = 0; i < teams; i++) {
+			standings[i] = new int[2];
+			standings[i][0] = i;
+			standings[i][1] = 0;
+		    }
 
-    int** display = new int* [teams];
+		    int** display = new int* [teams];
 
-    cout << "**\tIt's time to start the season's races!\t**" << endl;
-    cin>>dummy;
-    cout << "__________________________________________________________________________" << endl;
-    cout << endl;
+		    cout << "**\tIt's time to start the season's races!\t**" << endl;
+		    cout << "Type in 'Y' or 'N' please" << endl;
+		    cin>>dummy;
+		    if(dummy == "Y"){
+			    cout << "__________________________________________________________________________" << endl;
+			    cout << endl;
 
-    for (int x = 0; x < num; x++) {
+			    for (int x = 0; x < num; x++) {
 
-        cout << endl;
-        cout << "**Should we continue to the race?**" << endl;
-        cin >> dummy;
-        cout << "__________________________________________________________________________" << endl;
-        cout << endl;
+				cout << endl;
+				cout << "**Should we continue to the race?** Type 'Y' or 'N' " << endl;
+				cin >> dummy;
+				if(dummy == "Y"){
+					cout << "__________________________________________________________________________" << endl;
+					cout << endl;
 
-        cout << "\t<<<\tRace " << x+1 << " is ready to start\t>>>" << endl;
-        cout << "__________________________________________________________________________" << endl;
-        cout << endl;
-        results = iterator->currentItem()->startRace();
-        sorting = manager->sortResults(results, sorting, teams);
-        standings = manager->givePoints(sorting, standings, teams);
-        display = manager->sortResults(standings, teams);
+					cout << "\t<<<\tRace " << x+1 << " is ready to start\t>>>" << endl;
+					cout << "__________________________________________________________________________" << endl;
+					cout << endl;
+					results = iterator->currentItem()->startRace();
+					sorting = manager->sortResults(results, sorting, teams);
+					standings = manager->givePoints(sorting, standings, teams);
+					display = manager->sortResults(standings, teams);
 
-        cout << endl;
-        manager->display(display, teams);
-        iterator->next();
-    }
+					cout << endl;
+					manager->display(display, teams);
+					iterator->next();
+				}
+				else{
+					break;
+				}
+			    }
+		    
 
-    cout << "**\tA winner has been decided!\t**" << endl;
-    cin>>dummy;
-    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
-    cout << endl;
+			    cout << "**\tA winner has been decided!\t**" << endl;
+			    cout << ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<" << endl;
+			    cout << endl;
 
-    cout << "**\tThe winner is Team " << display[0][0]+1 << " with " << display[0][1] << " points!\t**" << endl;
+			    cout << "**\tThe winner is Team " << display[0][0]+1 << " with " << display[0][1] << " points!\t**" << endl;
+		    }
+		    else{
+			cout<<"The simulation has ended! Have a Nice day."<<endl;
+		    }
+	    }
+	else{
+		cout<<"Simulation ended. Have a Nice day!"<<endl;    
+	}
+    //return 0;
 }
 
 void RaceManager::display(int** standings, int num) {
